fc7b7e7abab145d7fae5ef73a9ffc95aef5ff3b4
@srivankit <ankits@mun.ca>
2020-07-13 21:13:11 -0230

    calculating mortgage

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,28 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+month = 0
+while principal > 0:
+    month = month+1
+    if(month >=extra_payment_start_month and month<=extra_payment_end_month):
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment + extra_payment
+        principal = principal - extra_payment
+        
+    else:
+        principal = principal * (1+rate/12) - payment
+        total_paid = total_paid + payment
+        
+        
+    print(month, round(total_paid,4), round(principal,2))
+
+
+print('Total paid', round(total_paid,2))
+print('Months', month)
