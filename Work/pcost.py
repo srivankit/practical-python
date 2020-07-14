eea08daeb5d178bc4c419a314493066e088519a6
@srivankit <ankits@mun.ca>
2020-07-14 18:50:57 -0230

    Exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,16 +1,20 @@
 # pcost.py
 #
 # Exercise 1.27
-Total_Cost=0
-shares_no = 0
-share_price = 0
-with open('Data/portfolio.csv','rt') as f:
-    next(f)
-    for line in f:
-        row = line.split(',')
-        share_price = row[2][:-1]
-        shares_no = row[1]
-        Total_Cost = Total_Cost+(int(shares_no)*float(share_price))
+def portfolio_cost(filename):
+    Total_Cost=0
+    shares_no = 0
+    share_price = 0
+    with open(filename,'rt') as f:
+        next(f)
+        for line in f:
+            row = line.split(',')
+            share_price = row[2][:-1]
+            shares_no = row[1]
+            Total_Cost = Total_Cost+(int(shares_no)*float(share_price))
+    return Total_Cost        
 
-print(f'Total cost {Total_Cost}')
+
+cost = portfolio_cost('Data/portfolio.csv')
+print(f'Total cost {cost}')
 
