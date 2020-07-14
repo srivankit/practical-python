e9145bcd861f9067fa347016eecf708039403de9
@srivankit <ankits@mun.ca>
2020-07-14 19:42:25 -0230

    Exercise 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,17 +1,25 @@
 # pcost.py
 #
 # Exercise 1.27
+'''
+Computes total cost of the portfolio price* no. of shares
+
+'''
 def portfolio_cost(filename):
-    Total_Cost=0
+    import csv
+    Total_Cost=0.0
     shares_no = 0
-    share_price = 0
+    share_price = 0.0
     with open(filename,'rt') as f:
-        next(f)
-        for line in f:
-            row = line.split(',')
-            share_price = row[2][:-1]
-            shares_no = row[1]
-            Total_Cost = Total_Cost+(int(shares_no)*float(share_price))
+        rows = csv.reader(f)
+        next(rows)
+        for row in rows:
+            try:
+                share_price = float(row[2])
+                shares_no = int(row[1])
+                Total_Cost = Total_Cost + (share_price  * shares_no)
+            except ValueError:
+                print("couldn't parse", row )    
     return Total_Cost        
 
 
