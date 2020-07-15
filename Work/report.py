c8fd9f65b94e99ea4a9f1ed1fb1bbe478adf2fc5
@srivankit <ankits@mun.ca>
2020-07-15 19:03:06 -0230

    Exercise 2.7: Finding out if you can retire

--- Work/report.py
+++ Work/report.py
@@ -2,24 +2,62 @@
 #
 # Exercise 2.4
 '''
-Computes total cost of the portfolio price* no. of shares
+Read stock proces into a list
 
 '''
-import sys
+
 def read_portfolio(filename):
     import csv
     Total_Cost=0.0
     portfolio = []
+    stock = {}
     with open(filename,'rt') as f:
         rows = csv.reader(f)
         next(rows) #skipping header
+        
         for row in rows:
             try:
-                holding = (row[0], int(row[1]), float(row[2]))
-                portfolio.append(holding)
+               stock ={
+                   'name':row[0],
+                   'shares': int(row[1]),
+                   'price': float(row[2])}
+                     
             except ValueError:
                 print('error in row')
-    return portfolio
-filename = 'Data/portfolio.csv'
-print(read_portfolio(filename))
+            
+            portfolio.append(stock) 
 
+    return portfolio
+
+
+def read_prices(filename):
+    prices ={}
+    import csv
+    with open(filename,'rt') as f:
+        rows = csv.reader(f)
+        
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+
+           
+    return prices
+
+portfolio = read_portfolio('Data/portfolio.csv')
+prices    = read_prices('Data/prices.csv')
+# print(prices)
+# print(portfolio)
+cost = 0
+sell = 0
+for row in portfolio:
+    cost +=row['shares']*row['price']
+    sell +=row['shares']*prices[row['name']]
+  
+if sell-cost >0:
+    print(f'Gain on the portfoli is {round((sell-cost),2)}')
+elif sell-cost<0:
+    print(f'Loss on the portfoli is {round((cost-sell),2)}')
+else:
+    print('No Profit or Loss on the portfolio')
