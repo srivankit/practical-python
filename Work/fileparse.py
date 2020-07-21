1d1b84c8150689a5fc7863c4d39e1e22c91835f2
@srivankit <ankits@mun.ca>
2020-07-21 17:58:34 -0230

    Exercise 3.6: Working without Headers

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,23 +3,26 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename, select = None, types = None):
+def parse_csv(filename, select = None, types = None, has_headers = True):
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
         rows = csv.reader(f)
 
-        # Read the file headers
-        headers = next(rows)
-        # If a column selector was given, find indices of the specified columns.
-        # Also narrow the set of headers used for resulting dictionaries
-        if select:
-            indices = [headers.index(colname) for colname in select]
-            headers = select
+        if has_headers ==True:
+
+            # Read the file headers
+            headers = next(rows)
+            # If a column selector was given, find indices of the specified columns.
+            # Also narrow the set of headers used for resulting dictionaries
+            if select:
+                indices = [headers.index(colname) for colname in select]
+                headers = select
+            else:
+                indices = []
         else:
             indices = []
-
         records = []
 
         for row in rows:
@@ -32,8 +35,8 @@ def parse_csv(filename, select = None, types = None):
             if indices:
                 row = [ row[index] for index in indices ]    
   
-            # Make a dictionary
-            record = dict(zip(headers, row))
+            # Make a tuple of records
+            record = tuple(row)
             records.append(record)
 
     return records
@@ -41,5 +44,6 @@ def parse_csv(filename, select = None, types = None):
 
 # portfolio = parse_csv('Data/portfolio.csv', select=['name','shares'])
 #portfolio = parse_csv('Data/portfolio.csv', types=[str, int, float])
-shares_held = parse_csv('Data/portfolio.csv', select=['name', 'shares'], types=[str, int])
-print(shares_held)
+# shares_held = parse_csv('Data/portfolio.csv', select=['name', 'shares'], types=[str, int])
+prices = parse_csv('Data/prices.csv', types=[str,float],has_headers=False)
+print(prices)
