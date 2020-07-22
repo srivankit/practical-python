a329607f79b28402a4caaa9ed2c36af9c537fa4f
@srivankit <ankits@mun.ca>
2020-07-22 12:01:48 -0230

    Exercise 3.17: From filenames to file-like objects

---
+++ Work/practice.py
@@ -0,0 +1,70 @@
+# practice.py
+#
+# Exercise 3.3
+import csv
+
+def parse_csv(filename, select = None, types = None, has_headers = True, delimiter=',', silence_errors=False):
+    '''
+    Parse a CSV file into a list of records
+    '''
+    from collections import Iterable
+    if select and has_headers ==False:
+            raise RuntimeError("select argument requires column headers")
+    if isinstance(filename, str):
+        f = open(filename, 'rt')
+        rows = csv.reader(f, delimiter=delimiter)             
+    else:
+
+        for lines in filename:
+            
+            rows = csv.reader(list(filename), delimiter = delimiter)
+            
+    if has_headers ==True:
+
+        # Read the file headers
+        headers = next(rows)
+        # If a column selector was given, find indices of the specified columns.
+        
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
+    else:
+        indices = []
+    records = []
+
+    for rownum, row in enumerate(rows,1):
+        if not row:    # Skip rows with no data
+            continue
+        
+        # If specific column indices are selected, pick them out
+        if indices:
+            row = [ row[index] for index in indices ]  
+        
+        # Apply type conversion to the row
+        if types:
+            try:
+                row = [func(val) for func, val in zip(types, row) ]
+            except ValueError as e:
+                if not silence_errors:
+                    print(f"Row {rownum}: Couldn't parse {row}")
+                    print(f'Row {rownum} {e}')
+
+    # Make a dictionary/tuple of records
+        if has_headers:
+            record = dict(zip(headers,row))
+        else:
+            record = tuple(row)  
+        records.append(record)
+    return records
+
+portfolio = parse_csv('Data/missing.csv', types=[str,int,float], silence_errors=True)
+print(portfolio)
+
+# import gzip
+# import practice 
+# with gzip.open('Data/portfolio.csv.gz', 'rt') as file:
+
+#     port = practice.parse_csv(file, types=[str,int,float])
+#     print(port)
